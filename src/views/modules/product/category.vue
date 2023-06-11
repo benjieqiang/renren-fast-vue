729f485d3f91fc78d8de1dac3ed3498cefcda8f5
benjieqiang <765126548@qq.com>
2023-06-11 16:58:00 +0800

    feat: 增加节点

--- src/views/modules/product/category.vue
+++ src/views/modules/product/category.vue
@@ -1,11 +1,12 @@
 <!--
  * @Author: benjie
  * @Date: 2023-06-11 13:02:17
- * @LastEditTime: 2023-06-11 16:11:38
+ * @LastEditTime: 2023-06-11 16:55:04
  * @LastEditors: benjie
  * @Description: 
 -->
 <template lang="">
+  <div>
     <el-tree    
       :data="menus"
       show-checkbox
@@ -37,13 +38,30 @@
       </span>
     </span>
     </el-tree>
+
+   <!--对话框组件-->
+	<el-dialog title="提示" :visible.sync="dialogVisible" width="30%">
+      <el-form :model="categroy">
+        <el-form-item label="分类名称">
+          <el-input v-model="categroy.name" autocomplete="off"></el-input>
+        </el-form-item>
+      </el-form>
+      <span slot="footer" class="dialog-footer">
+        <el-button @click="dialogVisible = false">取 消</el-button>
+        <el-button type="primary" @click="addCategory">确 定</el-button>
+      </span>
+    </el-dialog>
+  </div>
 </template>
+
 <script>
 export default {
   data() {
     return {
       menus: [],
       expandedKey: [],
+      dialogVisible: false, //对话框
+      categroy: { name: "", parentCid: 0, catLevel: 0, showStatus: 1, sort: 0 },
       defaultProps: {
         children: "children", //子节点
         label: "name", //name属性作为标签的值，展示出来
@@ -64,9 +82,14 @@ export default {
         this.menus = data.data;
       });
     },
+    // 添加节点
     append(data) {
       console.log("append", data);
+      this.dialogVisible = true;
+      this.categroy.parentCid = data.catId;
+      this.categroy.catLevel = data.catLevel * 1 + 1;
     },
+    // 删除子节点
     remove(node, data) {
       var ids = [data.catId];
       this.$confirm(`是否删除【${data.name}】菜单？`, "提示", {
@@ -90,10 +113,29 @@ export default {
             this.expandedKey = [node.parent.data.catId]
           });
         })
-        .catch(() => {});
+        .catch(() => { });
+    },
+    //点击确定后，发送post请求
+    //成功后显示添加成功，展开刚才的菜单
+    addCategory() {
+      console.log("提交的数据", this.categroy);
+      this.$http({
+        url: this.$http.adornUrl("/product/category/save"),
+        method: "post",
+        data: this.$http.adornData(this.categroy, false),
+      }).then(({ data }) => {
+        this.$message({
+          message: "添加成功",
+          type: "success",
+        });
+        //刷新出新的菜单
+        this.getMenus();
+        //设置需要默认展开的菜单
+        this.expandedKey = [this.categroy.parentCid];
+        this.dialogVisible = false;
+      });
     },
   },
-
   //监听属性 类似于data概念
   computed: {},
   //监控data中的数据变化
@@ -102,17 +144,8 @@ export default {
   created() {
     //创建完成时，就调用getMenus函数
     this.getMenus();
-  },
-  //生命周期 - 挂载完成（可以访问DOM元素）
-  mounted() { },
-  beforeCreate() { }, //生命周期 - 创建之前
-  beforeMount() { }, //生命周期 - 挂载之前
-  beforeUpdate() { }, //生命周期 - 更新之前
-  updated() { }, //生命周期 - 更新之后
-  beforeDestroy() { }, //生命周期 - 销毁之前
-  destroyed() { }, //生命周期 - 销毁完成
-  activated() { }, //如果页面有keep-alive缓存功能，这个函数会触发
-};
+  }
+}
 </script>
 <style lang="">
 </style>
